c91d93cbedbae37af16df709cae017aebaef8264
Cryptopranali <pranaliwadtile@gmail.com>
2026-03-09 10:30:36 +0530

    Create 01_variables.js

---
+++ .devcontainer/01.basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14556
+let accountEmail ="pranal@gmail.com"
+var accountPassword = "12345"
+accountCity ="Pune"
+
+// accountId = 2// not allowed beacuse it is const
+
+accountEmail = "hc@gmail.com"
+accountPassword ="233334"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
