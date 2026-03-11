151086ac8c0e52035bb30708c8e8f98b645cede1
Cryptopranali <pranaliwadtile@gmail.com>
2026-03-11 22:36:36 +0530

    Create 03_operations.js

---
+++ .devcontainer/01.basics/03_operations.js
@@ -0,0 +1,36 @@
+let someNumber = 33
+
+//**********************operations**************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "pranali"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+ 2)
+console.log(2+"2");//consider"" it as a string 
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 + 4 * 4 % 6);//use paranthesis every time you use multiple symbols in same statemnt 
+
+console.log(+true);// gives output as an 1 
+// console.log(true+);//gives an error
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
